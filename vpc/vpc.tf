d8e43377763f9aa1e6fff7d59ad22776f9e87098
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-20 21:11:30 +0530

    updating

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -11,5 +11,5 @@ resource "aws_vpc_ipv4_cidr_block_association" "private_cidr" {
   count      = length(var.private_vpc_cidr)
   vpc_id     = aws_vpc.main.id
  # cidr_block = element(var.private_vpc_cidr, count.index)
-  cidr_block = element(var.private_vpc_cidr)[count.index].rendered
+  cidr_block = element(var.private_vpc_cidr, count.index).rendered
 }
